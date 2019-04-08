b219bbdad2ab4c9b0162951a8169dc65a9952ad8
Gabe Velasquez <gvelasquez@gmail.com>
2019-04-08 16:14:06 -0700

    final working assignment

--- server.py
+++ server.py
@@ -1,19 +1,33 @@
 from flask import Flask, render_template, request, redirect
+import datetime
 app = Flask(__name__)  
 
-@app.route('/')         
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
     print(request.form)
     print('user POSTed checkout')
-    return render_template("checkout.html")
+    d = datetime.datetime.today()
+    data = {
+        'strawberry': request.form["strawberry"],
+        'raspberry': request.form["raspberry"],
+        'apple': request.form["apple"],
+        'blackberry': request.form["blackberry"],
+        'first_name': request.form["first_name"],
+        'last_name': request.form["last_name"],
+        'student_id': request.form["student_id"],
+        'num_items': int(request.form["strawberry"]) + int(request.form["raspberry"]) + int(request.form["apple"]) + int(request.form["blackberry"]),
+        'date_submitted': d.strftime("%B %d %Y %H:%M:%S")
+    }
+    print(data)
+    return render_template("checkout.html", order_info=data)
 
-@app.route('/fruits')         
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+if __name__=="__main__":
+    app.run(debug=True)
